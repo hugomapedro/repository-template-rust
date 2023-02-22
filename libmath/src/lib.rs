fcd83a8d918d080ac14729aedc844b4c28a8a4ac
hmapedro <78369129+hmapedro@users.noreply.github.com>
2023-02-22 14:51:55 +0000

    Master hp (#41)
    * feat(readme): test
    
    * fix(libmath): Change message on the add function
    
    * feat(libmath): add sub function

--- libmath/src/lib.rs
+++ libmath/src/lib.rs
@@ -2,3 +2,9 @@ pub fn add(a: i32, b: i32) -> i32 {
     println!("call add function");
     return a + b;
 }
+
+
+pub fn sub(a: i32, b: i32) -> i32 {
+    println!("call subtraction function");
+    return a - b;
+}
